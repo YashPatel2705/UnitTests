98d8a4d7b7979239b16df6aed53b0f780c8a6726
Yash Patel <itsyashu27@gmail.com>
2024-03-06 18:05:05 -0500

    First Unit Case

---
+++ TriangleTestCases/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+
+namespace TriangleTestCases
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        //UnitTest1
+        [Test]
+        public void ValidTriangle1_Input50and50and50_OutputValidTriangle()
+        {
+            //Arrange
+            int fAngle = 50, sAngle = 50, tAngle = 50;
+
+            string expected = "An equilateral triangle is formed";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(fAngle, sAngle, tAngle);
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
